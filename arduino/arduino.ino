5777905f0c2fa047ed6e592be2816df6438729c3
Liam Lalonde <71791017+llalon@users.noreply.github.com>
2020-11-12 11:56:32 -0500

    Minor revision

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,24 +1,24 @@
 #include <LiquidCrystal.h>
-#include "DHT.h"
+#include <DHT.h>
 
-#define DHTPIN A1 // define hardware
-#define DHTTYPE DHT11
-
-// temp/humi probe
-DHT dht(DHTPIN, DHTTYPE);
-
+// delay between readings (ms)
 int delayTime = 5000;
 
 // vars for LCD
 int Contrast = 50;
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2); 
 
+// sensor
+DHT dht(A1, DHT11);
+
 void setup() {
   Serial.begin(9600);
 
-  dht.begin(); // initialize the sensor
-  
-  analogWrite(6,Contrast); // LCD setup
+  // initialize the sensor
+  dht.begin();
+
+  // LCD setup
+  analogWrite(6,Contrast);
   lcd.begin(16, 2);
 
 }
@@ -27,31 +27,28 @@ void loop() {
   
   // read humidity
   float humi  = dht.readHumidity();
-  // read temperature as Celsius
-  float tempC = dht.readTemperature();
-  // read temperature as Fahrenheit
-  float tempF = dht.readTemperature(true);
+  // read temperature (C)
+  float temp = dht.readTemperature();
 
   // check if any reads failed
-  if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
+  if (isnan(humi) || isnan(temp)) {
     Serial.println("Failed to read from DHT sensor!");
   } else {
+    // print to serial. This will be read by the rpi
     Serial.print("Humidity: ");
     Serial.print(humi);
     Serial.print(" %");
     Serial.print("  |  "); 
     Serial.print("Temperature: ");
-    Serial.print(tempC);
-    Serial.print(" °C ~ ");
-    Serial.print(tempF);
-    Serial.println(" °F");
+    Serial.print(temp);
+    Serial.print(" °C");
 
     // print to LCD
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Temp: ");
     lcd.setCursor(6,0);
-    lcd.print(tempC,1);
+    lcd.print(temp,1);
     lcd.setCursor(11,0);
     lcd.print("C");
     lcd.setCursor(0, 1);
@@ -61,7 +58,7 @@ void loop() {
     lcd.setCursor(11,1);
     lcd.print("%");
 
-    // wait a few seconds between measurements.
+    // delay between readings
     delay(delayTime);
   }
 }
