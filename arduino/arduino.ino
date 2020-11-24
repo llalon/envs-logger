4bf61699ba31bd7046f38f7f9609f12d4a9a58ce
Liam <71791017+llalon@users.noreply.github.com>
2020-11-24 11:42:22 -0500

    Switch from ISS to CSV

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -6,39 +6,43 @@ int delayTime = 5000;
 
 // vars for LCD
 int Contrast = 50;
-LiquidCrystal lcd(9, 8, 5, 4, 3, 2); 
+LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
 
 // sensor
 DHT dht(A1, DHT11);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   // initialize the sensor
   dht.begin();
 
   // LCD setup
-  analogWrite(6,Contrast);
+  analogWrite(6, Contrast);
   lcd.begin(16, 2);
-
 }
 
-void loop() {
-  
+void loop()
+{
+
   // read humidity
-  float humi  = dht.readHumidity();
+  float humi = dht.readHumidity();
   // read temperature (C)
   float temp = dht.readTemperature();
 
   // check if any reads failed
-  if (isnan(humi) || isnan(temp)) {
+  if (isnan(humi) || isnan(temp))
+  {
     Serial.println("Failed to read from DHT sensor!");
-  } else {
+  }
+  else
+  {
     // print to serial. This will be read by the rpi
     Serial.print("Humidity: ");
     Serial.print(humi);
     Serial.print(" %");
-    Serial.print("  |  "); 
+    Serial.print("  |  ");
     Serial.print("Temperature: ");
     Serial.print(temp);
     Serial.print(" °C");
@@ -47,15 +51,15 @@ void loop() {
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("Temp: ");
-    lcd.setCursor(6,0);
-    lcd.print(temp,1);
-    lcd.setCursor(11,0);
+    lcd.setCursor(6, 0);
+    lcd.print(temp, 1);
+    lcd.setCursor(11, 0);
     lcd.print("C");
     lcd.setCursor(0, 1);
     lcd.print("RH: ");
-    lcd.setCursor(6,1);
-    lcd.print(humi,1);
-    lcd.setCursor(11,1);
+    lcd.setCursor(6, 1);
+    lcd.print(humi, 1);
+    lcd.setCursor(11, 1);
     lcd.print("%");
 
     // delay between readings
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from ISStreamer.Streamer import Streamer
 import time
 import serial
 import logging
@@ -9,16 +8,29 @@ logging.basicConfig(filename=settings.LOG_LOCATION, encoding='utf-8',
                     level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 
-# init isstreamer account
-streamer = Streamer(bucket_name=settings.BUCKET_NAME,
-                    bucket_key=settings.BUCKET_KEY, access_key=settings.ACCESS_KEY)
-
-
 def log_message(msg):
     """ Prints msg to console and log file
     """
     logging.info(msg)
     print(msg)
+
+
+def log_data(temp, hum, data_path):
+    """ Logs the inputed data to the csv
+    temp = temperature data (C)
+    hum = humidity data (%)
+    data_path = file path to save csv to
+    """
+
+    temp = format(temp, ".2f")
+    hum = format(hum, ".2f")
+
+    # log to csv
+    return(0)
+
+    # Data not logged return 1
+    log_message("ERROR: data unable to log")
+    return(1)
 
 
 if __name__ == "__main__":
@@ -30,7 +42,7 @@ if __name__ == "__main__":
     ser.flush()
 
     # Set variables to NA (999.0)
-    humidity = temp_c = 999.0
+    humidity = temperature = 999.0
 
     while True:
 
@@ -41,9 +53,9 @@ if __name__ == "__main__":
                 if ser.in_waiting > 0:
                     line = ser.readline().decode('utf-8').rstrip()
                     humidity = float(line.split(' ')[1])
-                    temp_c = float(line.split(' ')[7])
+                    temperature = float(line.split(' ')[7])
                     log_message("READIN:: Hum: " +
-                                str(humidity) + ", Temp: " + str(temp_c))
+                                str(humidity) + ", Temp: " + str(temperature))
                     log_message("done.")
                     break
                 else:
@@ -56,21 +68,8 @@ if __name__ == "__main__":
         if (humidity != 999.0):
             log_message("Sending temperature to server...")
 
-            # log temp
-            if settings.METRIC_UNITS:
-                streamer.log(settings.SENSOR_LOCATION_NAME +
-                             " Temperature(C)", temp_c)
-            else:
-                temp_f = format(temp_c * 9.0 / 5.0 + 32.0, ".2f")
-                streamer.log(settings.SENSOR_LOCATION_NAME +
-                             " Temperature(F)", temp_f)
-
-            # log humidity
-            log_message("Sending humidity to server...")
-            humidity = format(humidity, ".2f")
-            streamer.log(settings.SENSOR_LOCATION_NAME +
-                         " Humidity(%)", humidity)
-            streamer.flush()
+            # log data
+            log_data(temperature, humidity)
 
             log_message("Writting to log")
 
